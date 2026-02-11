b604bfa3e1a198d8366662c6636f5d99b2ff083f
Shraddha Kaspate <shraddhakaspate80@gmail.com>
2026-02-11 19:09:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Max distance between same elements/max-distance-between-same-elements.java
+++ Difficulty: Easy/Max distance between same elements/max-distance-between-same-elements.java
@@ -1,16 +1,38 @@
 class Solution {
     public int maxDistance(int[] arr) {
         // Code here
-        int max=0;
-        HashMap<Integer,Integer> map=new HashMap<>();
+        /*
+        int dist=0;
+        int maxdis=0;
+        
         for(int i=0;i<arr.length;i++){
-            if(!map.containsKey(arr[i])){
-                map.put(arr[i],i);
-            }else{
-                int dist=i-map.get(arr[i]);
-                max=Math.max(max,dist);
+            for(int j=i+1;j<arr.length;j++){
+                
+                if(arr[i]==arr[j]){
+                    dist=j-i;
+                    maxdis=Math.max(maxdis,dist);
+                }
+                
             }
         }
-        return max;
+        
+        return maxdis;
+        */
+        
+        int dist=0;
+        int maxdist=0;
+        
+        HashMap<Integer,Integer> map=new HashMap<>();
+        
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i])){
+                dist=i-map.get(arr[i]);
+                maxdist=Math.max(maxdist,dist);
+            }
+            else{
+                map.put(arr[i],i);
+            }
+        }
+        return maxdist;
     }
 }
