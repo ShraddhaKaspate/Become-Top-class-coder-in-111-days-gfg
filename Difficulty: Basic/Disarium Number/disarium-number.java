eca8221422893c38f891d2c35158e400cb412aae
Shraddha Kaspate <shraddhakaspate80@gmail.com>
2026-02-16 23:48:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Disarium Number/disarium-number.java
+++ Difficulty: Basic/Disarium Number/disarium-number.java
@@ -1,71 +1,28 @@
 // User function Template for Java
-/*
-class Solution
-{
-    static int isDisarium(int N)
-    {
-        // code here
-        int temp=N;
-        int r=0,digits=0,sum=0;
-        
-        while(temp>0)
-        {
-            temp=temp/10;
-            digits++;
-        }
-        
-        temp=N;
-        
-        while(temp>0)
-        {
-            for(int i=digits;i>=1;i--)
-            {
-       
-                 r=temp%10;
-                 temp=temp/10;
-                 sum=sum+(int)Math.pow(r,i);
-             }
-        }
-        System.out.println(sum);
-        if(sum==N)
-        {
-          System.out.println("yes");  
-        }
-        else
-        {
-            System.out.println("no");
-        }
-        
-        
-    }
-}*/
+
 class Solution {
     static int isDisarium(int N) {
-        int temp = N;
-        int digits = 0;
-
-        // Step 1: Count the number of digits
-        while (temp > 0) {
-            
-            temp /= 10;
-            digits++;
-        }
-
-        temp = N;
-        int sum = 0;
-
-        // Step 2: Calculate sum of digits raised to their respective positions
-        for (int i = digits; temp > 0; i--) {
-            int r = temp % 10;
-            temp /= 10;
-            sum += Math.pow(r, i);
-        }
-
-        // Step 3: Check if it's a Disarium number
-        if (sum == N) {
-            return 1; // Yes
-        } else {
-            return 0; // No
-        }
-    }
+        // code here
+      int temp=N;  
+      int sum=0;
+      int digit=0;
+      while(temp>0){
+          temp=temp/10;
+          digit++;
+      }
+      temp=N;
+      for(int i=digit;i>0;i--){
+          
+          while(temp > 0){
+    int rem = temp % 10;
+    sum = sum + (int)Math.pow(rem, i);
+    temp = temp / 10;
+    i--;
 }
+
+      }
+      
+      if(sum==N) return 1;
+      else return 0;
+    }
+};
