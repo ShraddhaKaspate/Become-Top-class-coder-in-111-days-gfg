6c2ed45f7c3efe9b046b9ac6b95e5198f6871f03
Shraddha Kaspate <shraddhakaspate80@gmail.com>
2026-03-16 17:24:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count of smaller elements/count-of-smaller-elements.java
@@ -0,0 +1,14 @@
+
+class Solution {
+    public int countOfElements(int x, List<Integer> arr) {
+        // Code here
+        int cnt=0;
+        Collections.sort(arr);
+        for(int i=0;i<arr.size();i++){
+            if(arr.get(i)<=x){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
