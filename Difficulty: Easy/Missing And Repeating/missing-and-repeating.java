61e5ec54f21b61479b6650bebc02b21502f96bcf
Shraddha Kaspate <shraddhakaspate80@gmail.com>
2026-04-17 15:20:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
@@ -1,23 +1,27 @@
 class Solution {
     ArrayList<Integer> findTwoElement(int arr[]) {
-        // code here
-        ArrayList<Integer> list=new ArrayList<>();
-        HashMap<Integer,Integer> map=new HashMap<>();
         
-        for(int i=0;i<arr.length;i++){
-            if(map.containsKey(arr[i])){
-                list.add(arr[i]);
+        ArrayList<Integer> ans = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+        int repeat = 0, missing = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            if(set.contains(arr[i])){
+                repeat = arr[i];
+            } else {
+                set.add(arr[i]);
             }
-            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
         
-        for(int i=1;i<=arr.length;i++){
-            if(!map.containsKey(i)){
-                list.add(i);
+        for(int i = 1; i <= arr.length; i++){
+            if(!set.contains(i)){
+                missing = i;
                 break;
             }
-            
         }
-        return list;
+        
+        ans.add(repeat);
+        ans.add(missing);
+        return ans;
     }
 }
