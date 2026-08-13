4f16243fe9d0936506fadf8f153677073d8f3e6e
Shraddha Kaspate <shraddhakaspate80@gmail.com>
2026-08-14 00:18:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.java
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.java
@@ -1,35 +1,31 @@
-// User function Template for Java
-
 class Solution {
     public int longestSubarray(int[] arr, int k) {
         // code here
-        int n=arr.length;
-        int []largest={-1,-1};
-        int largestlen=0;
-        int pref[]=new int[n];
-        HashMap<Integer,Integer> map=new HashMap<>();
         
-        pref[0]=arr[0];
-        for(int i=1;i<n;i++){
-            pref[i]=pref[i-1]+arr[i];
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int prefsum[]=new int[arr.length];
+        int maxsum=0;
+        prefsum[0]=arr[0];
+        
+        for(int i=1;i<arr.length;i++){
+            prefsum[i]=prefsum[i-1]+arr[i];
         }
         
         map.put(0,-1);
-        for(int i=0;i<n;i++){
-            if(map.containsKey(pref[i]-k)){
-                int len=i-map.get(pref[i]-k);
-                if(len>=largestlen){
-                    largestlen=len;
-                    largest[0]=map.get(pref[i]-k)+1;
-                    largest[1]=i;
-                }
-            }
-            if(!map.containsKey(pref[i])){
-                map.put(pref[i],i);
-            }
-            //System.out.print(largestlen);
-        }
-        return largestlen;
         
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(prefsum[i]-k)){
+                
+                int length = i - map.get(prefsum[i] - k);
+                maxsum=Math.max(maxsum,length);
+                
+            }
+            
+             if(!map.containsKey(prefsum[i])) {
+                map.put(prefsum[i], i);
+            }
         }
+        return maxsum;
+    }
+    
 }
